665b1f7e9edf5c5c2e049344783eb27bc54f3862
james jomuad <st.james.jomuad@gmail.com>
2019-11-17 15:15:50 +0800

    update git ignore

--- plugins/bookrr/store/Plugin.php
+++ plugins/bookrr/store/Plugin.php
@@ -117,18 +117,18 @@ class Plugin extends PluginBase
 
     public function registerNavigation()
     {
-        if(BackendAuth::getUser()->isCustomer())
-        {
-            return [
-                'car-rental' => [
-                    'label'       => 'Car Rental',
-                    'url'         => Backend::url('bookrr/store/carrent'),
-                    'icon'        => 'icon-car',
-                    'permissions' => ['bookrr.store.car-rent'],
-                    'order'       => 910,
-                ]
-            ];
-        }
+        // if(BackendAuth::getUser()->isCustomer())
+        // {
+        //     return [
+        //         'car-rental' => [
+        //             'label'       => 'Car Rental',
+        //             'url'         => Backend::url('bookrr/store/carrent'),
+        //             'icon'        => 'icon-car',
+        //             'permissions' => ['bookrr.store.car-rent'],
+        //             'order'       => 910,
+        //         ]
+        //     ];
+        // }
 
         return [
             'store' => [
